f269157eba7edc775a53dc27198f8e4bdcdf27b0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-10-19 21:04:05 +0200

    use unit-dependent functions

--- src/components/DailyItem.js
+++ src/components/DailyItem.js
@@ -1,22 +1,20 @@
 import React from "react"
 import CurrentImage from "./CurrentImage"
 import {
-  getCelsius,
-  getFahrenheit,
+  getTemperature,
   getPercent,
-  getMiles,
-  getInch,
+  getLength,
+  getPrecip,
   getDate,
   getTime,
 } from "./helpers"
 import { getMoonPhase } from "./moon"
 import {getDirection,
   getBeaufort,
-  getKph,
-  getMph,} from "./wind"
+  getVelocity} from "./wind"
 // import "../styles/DailyItem.scss";
 
-export default function DailyItem({ data, all }) {
+export default function DailyItem({ data, all, unit }) {
   const {
     time,
     icon,
@@ -58,52 +56,45 @@ export default function DailyItem({ data, all }) {
       <ul>
         <li>{summary}</li>
         <li>
-          maximum: {getCelsius(temperatureMax)}/{getFahrenheit(temperatureMax)}{" "}
+          maximum: {getTemperature(temperatureMax, unit)}{" "}
           at {getTime(temperatureMaxTime, timezone)}
         </li>
         <li>
-          minimum: {getCelsius(temperatureMin)}/{getFahrenheit(temperatureMin)}{" "}
+          minimum: {getTemperature(temperatureMin, unit)}{" "}
           at {getTime(temperatureMinTime, timezone)}
         </li>
         <li>
-          apparent maximum: {getCelsius(apparentTemperatureMax)}/
-          {getFahrenheit(apparentTemperatureMax)} at{" "}
+          apparent maximum: {getTemperature(apparentTemperatureMax, unit)} at{" "}
           {getTime(apparentTemperatureMaxTime, timezone)}
         </li>
         <li>
-          apparent minimum: {getCelsius(apparentTemperatureMin)}/
-          {getFahrenheit(apparentTemperatureMin)} at{" "}
+          apparent minimum: {getTemperature(apparentTemperatureMin, unit)} at{" "}
           {getTime(apparentTemperatureMinTime, timezone)}
         </li>
         <li>cloud cover: {getPercent(cloudCover)}</li>
         <li>precipitation probability: {getPercent(precipProbability)}</li>
         <li>
-          daily precipitation: {(24 * precipIntensity).toFixed(2)} mm/
-          {getInch(24 * precipIntensity)}
+          daily precipitation: {getPrecip(24 * precipIntensity, unit)}
         </li>
         <li>
-          maximal precipitation: {precipIntensityMax.toFixed(2)} mm/
-          {getInch(precipIntensityMax)} at{" "}
+          maximal precipitation: {getPrecip(precipIntensityMax, unit)} at{" "}
           {getTime(precipIntensityMaxTime, timezone).substr(11)}
         </li>
         {precipType !== undefined && <li>precipitation type: {precipType}</li>}
         <li>air pressure: {pressure.toFixed(1)} mbar</li>
         <li>wind direction: {getDirection(windBearing)}</li>
         <li>
-          wind speed: {getBeaufort(windSpeed)}/{getKph(windSpeed)}/
-          {getMph(windSpeed)}
+          wind speed: {getBeaufort(windSpeed)} ({getVelocity(windSpeed, unit)})
         </li>
         <li>
-          wind gusts: {getBeaufort(windGust)}/{getKph(windGust)}/
-          {getMph(windGust)} at {getTime(windGustTime, timezone).substr(11)}
+          wind gusts: {getBeaufort(windGust)} ({getVelocity(windGust, unit)}) at {getTime(windGustTime, timezone).substr(11)}
         </li>
         <li>relative humidity: {getPercent(humidity)}</li>
         <li>
-          dew point: {getCelsius(dewPoint)}/{getFahrenheit(dewPoint)}
+          dew point: {getTemperature(dewPoint, unit)}
         </li>
         <li>
-          visibility: {visibility.toFixed(1)} km/
-          {getMiles(visibility)}
+          visibility: {getLength(visibility, unit)}
         </li>
         <li>UV index: {uvIndex}</li>
         <li>sunrise at {getTime(sunriseTime, timezone).substr(11)}</li>
