b71c5b8394f6e60c4dd72c9424540a2ec4c0cd22
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-10-20 07:27:59 +0200

    use unit-dependent functions in HourlyItem

--- src/components/HourlyItem.js
+++ src/components/HourlyItem.js
@@ -1,67 +1,60 @@
 import React from "react"
 import CurrentImage from "./CurrentImage"
 import {
-  getCelsius,
-  getFahrenheit,
+  getTemperature,
   getPercent,
-  getMiles,
-  getInch,
+  getLength,
+  getPrecip,
   getTime,
 } from "./helpers"
 import {getDirection,
   getBeaufort,
-  getKph,
-  getMph,} from "./wind"
+  getVelocity} from "./wind"
 import "../styles/HourlyItem.scss"
 
-export default function HourlyItem(props) {
+export default function HourlyItem({data, all, unit}) {
+  const { time, icon, summary, temperature, apparentTemperature, cloudCover, precipProbability, precipIntensity, precipType, pressure, windBearing, windSpeed, windGust, humidity, dewPoint, visibility, uvIndex } = data
+  const { timezone } = all
   return (
     <div className="col-lg-4 col-md-6 col-sm-12">
-      <h2>{getTime(props.data.time, props.all.timezone)}</h2>
+      <h2>{getTime(time, timezone)}</h2>
       <div className="imag">
-        <CurrentImage icon={props.data.icon}  style={{ fontSize: "96px" }} />
+        <CurrentImage icon={icon}  style={{ fontSize: "96px" }} />
       </div>
       <ul>
-        <li>{props.data.summary}</li>
+        <li>{summary}</li>
         <li>
-          temperature: {getCelsius(props.data.temperature)}/
-          {getFahrenheit(props.data.temperature)}
+          temperature: {getTemperature(temperature, unit)}
         </li>
         <li>
-          feels like: {getCelsius(props.data.apparentTemperature)}/
-          {getFahrenheit(props.data.apparentTemperature)}
+          feels like: {getTemperature(apparentTemperature, unit)}
         </li>
-        <li>cloud cover: {getPercent(props.data.cloudCover)}</li>
+        <li>cloud cover: {getPercent(cloudCover)}</li>
         <li>
-          precipitation probability: {getPercent(props.data.precipProbability)}
+          precipitation probability: {getPercent(precipProbability)}
         </li>
-        <li>
-          hourly precipitation: {props.data.precipIntensity} mm/
-          {getInch(props.data.precipIntensity)}
-        </li>
-        {props.data.precipType !== undefined && (
-          <li>precipitation type: {props.data.precipType}</li>
+        {precipIntensity > 0 && (<li>
+          hourly precipitation: {getPrecip(precipIntensity, unit)}
+        </li>)}
+        {precipType !== undefined && (
+          <li>precipitation type: {precipType}</li>
         )}
-        <li>air pressure: {props.data.pressure.toFixed(1)} mbar</li>
-        <li>wind direction: {getDirection(props.data.windBearing)}</li>
+        <li>air pressure: {pressure.toFixed(1)} mbar</li>
+        <li>wind direction: {getDirection(windBearing)}</li>
         <li>
-          wind speed: {getBeaufort(props.data.windSpeed)}/
-          {getKph(props.data.windSpeed)}/{getMph(props.data.windSpeed)}
+          wind speed: {getBeaufort(windSpeed)} ({getVelocity(windSpeed, unit)})}
         </li>
         <li>
-          wind gusts: {getBeaufort(props.data.windGust)}/
-          {getKph(props.data.windGust)}/{getMph(props.data.windGust)}
+          wind gusts: {getBeaufort(windGust)} ({getVelocity(windGust, unit)})
         </li>
-        <li>relative humidity: {getPercent(props.data.humidity)}</li>
+        <li>relative humidity: {getPercent(humidity)}</li>
         <li>
-          dew point: {getCelsius(props.data.dewPoint)}/
-          {getFahrenheit(props.data.dewPoint)}
+          dew point: {getTemperature(dewPoint, unit)}
         </li>
         <li>
-          visibility: {props.data.visibility.toFixed(1)} km/
-          {getMiles(props.data.visibility)}
+          visibility: {getLength(visibility, unit)}
         </li>
-        <li>UV index: {props.data.uvIndex}</li>
+        <li>UV index: {uvIndex}</li>
       </ul>
     </div>
   )
