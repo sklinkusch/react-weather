060ee04ed97ee0aa91840c8e1a1eabcd8f021f10
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-10-24 11:55:16 +0200

    include fontawesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import "bootstrap/scss/bootstrap.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import AppProvider from './context/AppProvider';
