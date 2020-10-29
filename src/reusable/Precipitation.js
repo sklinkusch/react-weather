767811b4b1602db1c436bbfb38af9fc0583ecf11
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-10-29 17:25:20 +0100

    include rain warning

--- src/reusable/Precipitation.js
+++ src/reusable/Precipitation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getPrecip, getTime } from '../components/helpers'
+import { RainWarning } from '../components/Warning'
 
 const Precipitation = ({term, intensity, unit, time = undefined, zone = undefined}) => {
   if (intensity === 0){
@@ -8,13 +9,13 @@ const Precipitation = ({term, intensity, unit, time = undefined, zone = undefine
   if (time && zone) {
     return (
       <li>
-        {term}: {getPrecip(intensity, unit)} at {getTime(time, zone).substr(11)}
+        {term}: {getPrecip(intensity, unit)} at {getTime(time, zone).substr(11)} <RainWarning precipIntensity={intensity} />
       </li>
     )
   }
   return (
     <li>
-      {term}: {getPrecip(intensity, unit)}
+      {term}: {getPrecip(intensity, unit)} <RainWarning precipIntensity={intensity} />
     </li>
   )
 }
