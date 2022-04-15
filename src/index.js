2b837ffdb28552f2b35497b2317d42ebcfd6bee5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-04-15 11:43:35 +0200

    change fontawesome to js import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import AppProvider from './context/AppProvider';
